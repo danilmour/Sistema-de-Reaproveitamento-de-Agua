78a435cf27ee1d5154c3b5190c82fe11b36017a6
danilmour <81158510+danilmour@users.noreply.github.com>
2022-11-16 18:46:38 +0000

    Changed name to teste-reles

--- teste-reles/src/main.cpp
+++ teste-reles/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(A15, OUTPUT);
+  pinMode(15, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(A15, HIGH);
+  digitalWrite(15, HIGH);
   sleep(1);
-  digitalWrite(A15, LOW);
+  digitalWrite(15, LOW);
   sleep(1);
 }
