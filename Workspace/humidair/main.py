17b3ab9eacd3df7ba2815356f49d2e393014548a
eddygrinder <118298931+eddygrinder@users.noreply.github.com>
2022-12-02 01:41:42 +0000

    Update main.py

--- Workspace/humidair/main.py
+++ Workspace/humidair/main.py
@@ -3,26 +3,51 @@ from machine import Pin
 import bme280
 from hcsr04 import HCSR04
 
-# Led nível superior ao minimo
-led_amarelo = Pin(21, Pin.OUT)
-# Led menor que nível minimo
-led_azul = Pin(4, Pin.OUT)
+led_amarelo = Pin(21, Pin.OUT)  # Led nível superior ao minimo
+led_azul = Pin(4, Pin.OUT)  # Led menor que nível minimo
 
+# Leitura dos estados dos contactos auxiliares de disjuntores
+Cnt_Dis_C1 = Pin(13, Pin.IN)  # Contacto auxiliar - Motor 1
+Cnt_Dis_C2 = Pin(12, Pin.IN)  # Contacto auxiliar - Motor 2
+
+# Leitura dos tempos de funcionamento dos motores
+Cnt_Aux_C1 = Pin(14, Pin.IN)  # Contacto auxiliar - Motor 1
+Cnt_Aux_C2 = Pin(27, Pin.IN)  # Contacto auxiliar - Motor 2
 
 """
-Cálculo temperatura e humidade
-Bloco desumidificação
+  Cálculo temperatura e humidade
+  Bloco desumidificação
 """
 
+
+def CntAux():
+  if Cnt_Dis_C1.value() == 0:
+    print('Possível avaria MOTOR1')
+  else:
+    print('Motor 1 OK')
+
+  if Cnt_Dis_C2.value() == 0:
+    print('Possível avaria MOTOR2')
+  else:
+    print('Motor 2 OK')
+  
+  print('')
+  
+  if Cnt_Aux_C1.value() == 0:
+    print('Motor 1 Parado')
+  else:
+    print('Motor 1 em funcionamento')
+  if Cnt_Aux_C2.value() == 0:
+    print('Motor 2 Parado')
+  else:
+    print('Motor 2 em funcionamento')
+
+  print('')
 
 def temperatura():
   try:
     # Connect to BME-280 via software SPI with custom pinning (not supported by all microcontrollers):
-    bme = bme280.BME280(spiBus={ "sck": 18, "mosi": 23, "miso": 19 }, spiCS=5)
-
-    # Alternatively:
-    # Connect to BME-280 via pre-initialized Micropython bus object:
-    #bme = bme280.BME280(spiBus=machine.SPI(0, ...), spiCS=machine.Pin(5, ...))
+    bme = bme280.BME280(spiBus={"sck": 18, "mosi": 23, "miso": 19}, spiCS=5)
 
     # Synchronously trigger a MODE_FORCED conversion and return the result.
     temperature, humidity, pressure = bme.readForced(filter=bme280.FILTER_2,
@@ -38,7 +63,6 @@ def temperatura():
   except bme280.BME280Error as e:
     print(f"BME280 error: {e}")
 
-"""
 #### Cálculo do nível da água no depósito ####
 ##############################################
 ########################################
@@ -46,7 +70,7 @@ def temperatura():
 # Bloco armazenamento
 #
 #########################################
-"""
+
 
 def nivel():
   # Complete project details at https://RandomNerdTutorials.com/micropython-hc-sr04-ultrasonic-esp32-esp8266/
@@ -60,6 +84,7 @@ def nivel():
   if distance > 5 and distance < 20:
     led_amarelo.value(1)
     led_azul.value(0)
+    print('tanque entre valor minimo e maximo')
 
   ##
   # Limite minimo - depósito vazio
@@ -67,17 +92,19 @@ def nivel():
   if distance > 20:
     led_amarelo.value(0)
     led_azul.value(1)
+    print('tanque no valor minimo')
 
 ##
   # Só para uma questão de simulação
   # distancia maior que 20 apaga os leds
   if distance < 5:
-    led_amarelo.value(0)
-    led_azul.value(0)
-
+    led_amarelo.value(1)
+    led_azul.value(1)
+    print('tanque maximo')
   sleep(1)
 
 
 while True:
   nivel()
   temperatura()
+  CntAux()
