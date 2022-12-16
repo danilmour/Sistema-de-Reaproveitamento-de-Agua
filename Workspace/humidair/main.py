56ab573c8ab3d6c698f6d785ddd78b3628dc4526
eddygrinder <1210171@isep.ipp.pt>
2022-12-16 19:25:15 +0000

    Arranjo do código html

--- Workspace/humidair/main.py
+++ Workspace/humidair/main.py
@@ -169,48 +169,37 @@ def web_page(n):
 
     Cntagua()
 
-    <html>
+    html = """<html>
 
-    <head>
-    <title>HumidAir</title>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta http-equiv="refresh" content="5" name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <style>
-        body,
-        td {
-            text-align: center;
-        }
+                <head>
+                    <title>HumidAir</title>
+                    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+                    <meta http-equiv="refresh" content="5" name="viewport" content="width=device-width, initial-scale=1">
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+                    <style>
+                        body,
+                        td {
+                            text-align: center;
+                        }
 
-        .buttons {
-            width: 150px;
-        }
+                        .buttons {
+                            width: 150px;
+                        }
 
-        p,
-        td {
-            font-size: 15pt;
-        }
+                        p,
+                        td {
+                            font-size: 15pt;
+                        }
 
-        h1,
-        h2 {
-            color: darkblue;
-        }
+                        h1,
+                        h2 {
+                            color: darkblue;
+                        }
 
-<<<<<<< HEAD:Backup_APAGAR/main.py
-        .alerta {
-            color: red;
-        }
-
-        .espaco {
-            height: 100px;
-        }
-    </style>
-</head>
-=======
                         .alerta {
                             color: red;
                         }
-
+                        
                         .espaco {
                             height: 100px;
                         }
@@ -275,9 +264,9 @@ def web_page(n):
                     </table>
                     <div class="espaco"></div>
                     <table class="table" align="center">
-                        <tr>
+                        <thead>
                             <h2 class="alerta">Paragem de Emergência</h2>
-                        </tr>
+                        </thead>
                         <tr>
                             <td class="alerta">
                                 <b>! Desligar Desumidificador - SOS !</b>
@@ -298,91 +287,8 @@ def web_page(n):
                         </tr>
                     </table>
                 </body>
->>>>>>> 31ab0a0ceceda717004c13d18f22bb6c1b725bd3:Workspace/humidair/main.py
 
-<body>
-    <h1><b>HumidAir</b></h1>
-    <hr>
-    <table class="table">
-        <thead>
-            <h2>Informações</h2>
-        </thead>
-        <tr>
-            <td>
-                <b>Desumidificador</b>
-            </td>
-            <td>
-                """ + gpio_state + """
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <b>Temperatura</b>
-            </td>
-            <td>
-                """ + str(temperatura) + """ ºC
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <b>Humidade</b>
-            </td>
-            <td>
-                """ + str(humidade) + """ %
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <b>Estado do Depósito</b>
-            </td>
-            <td>
-                """ + estado_deposito + """
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <b>Tempo de funcionamento da Bomba 1</b>
-            </td>
-            <td>
-                """ + str(tempoTotal1) + """ s
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <b>Tempo de funcionamento da Bomba 2</b>
-            </td>
-            <td>
-                """ + str(tempoTotal2) + """ s
-            </td>
-        </tr>
-    </table>
-    <div class="espaco"></div>
-    <table class="table" align="center">
-        <tr>
-            <h2 class="alerta">Paragem de Emergência</h2>
-        </tr>
-        <tr>
-            <td class="alerta">
-                <b>! Desligar Desumidificador - SOS !</b>
-            </td>
-            <td>
-                <a href="/?led=on"><button class="btn btn-outline-success fs-1 buttons">ON</button></a>
-                <a href="/?led=off"><button class="btn btn-outline-danger fs-1 buttons">OFF</button></a>
-            </td>
-        </tr>
-        <tr>
-            <td class="alerta">
-                <b>! Desligar Central - SOS !</b>
-            </td>
-            <td>
-                <a href="/?central=on"><button class="btn btn-outline-success fs-1 buttons">ON</button></a>
-                <a href="/?central=off"><button class="btn btn-outline-danger fs-1 buttons">OFF</button></a>
-            </td>
-        </tr>
-    </table>
-</body>
-
-</html>
+                </html>"""
     return html
 
 
