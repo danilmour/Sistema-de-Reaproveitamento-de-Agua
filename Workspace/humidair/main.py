fd9caaa051fa123d1e15323ae4c06a8f80082a70
eddygrinder <118298931+eddygrinder@users.noreply.github.com>
2022-12-24 19:38:18 +0000

    update main.py - botões on/off
    o programa não funciona quando se actua nos botões no momento em que o código executa a função do BME

--- Workspace/humidair/main.py
+++ Workspace/humidair/main.py
@@ -1,51 +1,3 @@
-import utime as time
-#from time import sleep, time
-from bme280 import BME280
-
-from machine import Pin, ADC, RTC
-from hcsr04 import HCSR04
-import bme280
-
-led_amarelo = Pin(21, Pin.OUT)  # Led nível superior ao minimo
-led_azul = Pin(4, Pin.OUT)  # Led menor que nível minimo
-
-# Leitura dos estados dos contactos auxiliares de disjuntores
-Cnt_Dis_C1 = Pin(13, Pin.IN)  # Contacto auxiliar - Motor 1
-Cnt_Dis_C2 = Pin(12, Pin.IN)  # Contacto auxiliar - Motor 2
-
-# Leitura dos tempos de funcionamento dos motores
-Motor_1 = Pin(14, Pin.OUT)  # Contacto auxiliar - Motor 1
-Motor_2 = Pin(27, Pin.OUT)  # Contacto auxiliar - Motor 2
-
-# Potenciometros que simulam os contadores da água
-Cnt_Agua_sys = ADC(Pin(32))  # Contador água do sistema
-Cnt_Agua_sys.atten(ADC.ATTN_11DB)  # Full range: 3.3v
-# Cnt_Agua_rede = ADC(Pin(33))  # Contaágua da rede
-# Cnt_Agua_rede.atten(ADC.ATTN_11DB)  # Full range: 3.3v
-
-# Controlo do desumidificador - ON/OFF
-Cnt_desHUM = Pin(15, Pin.OUT)  # Led nível superior ao minimo
-Cnt_desHUM.value(0)
-
-# Simulação Válvulas 3 vias
-Cnt_V3V = Pin(2, Pin.OUT)
-Cnt_V3V.value(0)  # Válvula de 3 vias fechada - enche depósito
-
-cnt = 0
-toggle = 0
-soma = 0
-aux = 0
-tempoTotal1 = 0
-tempoTotal2 = 0
-
-# Controlo da eletroválvula
-# Se Estado_EletroValvula = 1 -> Sistema Humidair
-# Se Estado_EletroValvula = 0 -> Água da rede
-
-Estado_EletroValvula = 0  # Inicio sitema sanitário alimentado pela rede
-Cnt_EletroValvula = Pin(33, Pin.OUT)
-Cnt_EletroValvula.value(0)
-
 #####################################
 # Informação web-server             #
 #####################################
@@ -53,15 +5,25 @@ Cnt_EletroValvula.value(0)
 #####################################
 
 
-def pedido(s):
+def pedido(s, n, m):
     conn, addr = s.accept()
     print('Got a connection from %s' % str(addr))
-    request = conn.recv(1024)
+    request = conn.recv(2048)
     request = str(request)
     print('Content = %s' % request)
-    deshum_on = request.find('/?led=on')
-    deshum_off = request.find('/?led=off')
-    return deshum_on, deshum_off, conn
+    deshum_on = request.find('/?des=on')
+    deshum_off = request.find('/?des=off')
+    central_on = request.find('/?cen=on')
+    central_off = request.find('/?cen=off')
+    
+    response = web_page(n, m)  # Chama a função web_page
+    conn.send('HTTP/1.1 200 OK\r\n')
+    conn.send('Content-Type: text/html\r\n')
+    conn.send('Connection: close\n\n')
+    print ('bla')
+    conn.sendall(response)
+    conn.close()
+    return  deshum_on, deshum_off, central_on, central_off
 
 #####################################
 # Informação web-server             #
@@ -70,14 +32,15 @@ def pedido(s):
 #####################################
 
 
-def refresh(conn, n):
-    response = web_page(n)  # Chama a função web_page
+def refresh(n, m):
+    conn, addr = s.accept()    
+    response = web_page(n, m)  # Chama a função web_page
     conn.send('HTTP/1.1 200 OK\n')
     conn.send('Content-Type: text/html\n')
     conn.send('Connection: close\n\n')
+    print ('bla')
     conn.sendall(response)
     conn.close()
-
 
 ##########################################
 # Informação desumidificador/temperatura #
@@ -90,6 +53,7 @@ rtc = RTC()
 
 
 def Read_BME(n):
+    #print ('bme_n', n)
     try:
         # Synchronously trigger a MODE_FORCED conversion and return the result.
         temperature, humidity, pressure = bme.readForced(filter=bme280.FILTER_2,
@@ -98,15 +62,17 @@ def Read_BME(n):
                                                          pressureOversampling=bme280.OVSMPL_4)
 
         print(
-            f"{temperature:.1f} *C; {humidity * 100:.1f} % rel. hum.; {pressure / 100:.1f} hPa")
+             f"{temperature:.1f} *C; {humidity * 100:.1f} % rel. hum.; {pressure / 100:.1f} hPa")
 
     except bme280.BME280Error as e:
         print(f"BME280 error: {e}")
-
+    
+    
     if humidity * 100 > 60 and n == 1:  # MUDAR ISTO
         Cnt_desHUM.value(1)
-    else:
+    elif humidity * 100 < 60 or n == 0:
         Cnt_desHUM.value(0)
+
 
     return temperature, humidity*100
 
@@ -118,6 +84,8 @@ def Read_BME(n):
 
 
 def nivel():
+    global Estado_EletroValvula
+    
     sensor = HCSR04(trigger_pin=25, echo_pin=26, echo_timeout_us=10000)
 
     distancia = sensor.distance_cm()
@@ -130,7 +98,7 @@ def nivel():
         led_amarelo.value(1)
         led_azul.value(0)
         Cnt_V3V.value(0)  # Válvula de 3 vias fechada - enche depósito
-        Estado_EletroValvula = 1  # Sistema alimentado pelo Humidair
+        Estado_EletroValvula = 0  # Sistema alimentado pelo Humidair
 
     # Limite minimo - depósito vazio
     # distancia entre 18 e 20cm
@@ -141,7 +109,7 @@ def nivel():
         led_amarelo.value(0)
         led_azul.value(1)
         Cnt_V3V.value(0)  # Válvula de 3 vias fechada - enche depósito
-        Estado_EletroValvula = 0  # sistema alimentado pela rede
+        Estado_EletroValvula = 1  # sistema alimentado pela rede
 
     # Só para uma questão de simulação - TANQUE COMPLETAMENTE CHEI0
     # distancia maior que 20 apaga os leds
@@ -152,9 +120,7 @@ def nivel():
         led_amarelo.value(1)
         led_azul.value(1)
         Cnt_V3V.value(1)  # Válvula 3 vias aberta - água fora
-        Estado_EletroValvula = 1  # Sistema alimentado pelo Humidair
-
-    # time.sleep(1)
+        Estado_EletroValvula = 0  # Sistema alimentado pelo Humidair
 
     return nivel_maximo, nivel_medio, nivel_minimo
 
@@ -163,12 +129,23 @@ def nivel():
 ########################
 
 
-def web_page(n):
+def web_page(n, m):
+    global time_start
+    global gpio_state
+    global gpio_state2
+
     if n == 1:
         gpio_state = "ON"
-    else:
+    elif n == 0:
         gpio_state = "OFF"
-
+        
+    if m == 1:
+        gpio_state2 = "ON"
+        Cnt_Central.value(1)
+    elif m == 0:
+        gpio_state2 = "OFF"
+        Cnt_Central.value(0)
+    
     temperatura, humidade = Read_BME(n)
     nivel_maximo, nivel_medio, nivel_minimo = nivel()
 
@@ -178,15 +155,25 @@ def web_page(n):
         estado_deposito = "Medio"
     elif nivel_minimo == 1:
         estado_deposito = "vazio"
-
-    Cntagua()
+        
+    if Cnt_Dis_C1.value() == 1:
+       estado_motor1 = "OK"
+    elif Cnt_Dis_C1.value() == 0:
+       estado_motor1 = "Verificar Motor"
+    if Cnt_Dis_C2.value() == 1:
+       estado_motor2 = "OK"
+    elif Cnt_Dis_C2.value() == 0:
+       estado_motor2 = "Verificar Motor"
+    
+    time_start = rtc.datetime()
+    time.sleep (1)
 
     html = """<html>
 
                 <head>
                     <title>HumidAir</title>
                     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-                    <meta http-equiv="refresh" content="5" name="viewport" content="width=device-width, initial-scale=1">
+                    <meta http-equiv="refresh" content="2" name="viewport" content="width=device-width, initial-scale=1">
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
                     <style>
                         body,
@@ -235,6 +222,14 @@ def web_page(n):
                         </tr>
                         <tr>
                             <td>
+                                <b>Central</b>
+                            </td>
+                            <td>
+                                """ + gpio_state2 + """
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
                                 <b>Temperatura</b>
                             </td>
                             <td>
@@ -278,7 +273,7 @@ def web_page(n):
                                 <b>Estado da Eletroválvula</b>
                             </td>
                             <td>
-                                """ + Estado_EletroValvula + """
+                                """ + str(Estado_EletroValvula) + """
                             </td>
                         </tr>
                         <tr>
@@ -286,7 +281,7 @@ def web_page(n):
                                 <b>Estado do Motor 1</b>
                             </td>
                             <td>
-                                """ + Cnt_Dis_C1.value() + """
+                                """ + estado_motor1 + """
                             </td>
                         </tr>
                         <tr>
@@ -294,7 +289,7 @@ def web_page(n):
                                 <b>Estado do Motor 2</b>
                             </td>
                             <td>
-                                """ + Cnt_Dis_C2.value() + """                
+                                """ + estado_motor2 + """                
                             </td>
                         </tr>
                     </table>
@@ -308,8 +303,8 @@ def web_page(n):
                                 <b>! Desligar Desumidificador - SOS !</b>
                             </td>
                             <td>
-                                <a href="/?led=on"><button class="btn btn-outline-success fs-1 buttons">ON</button></a>
-                                <a href="/?led=off"><button class="btn btn-outline-danger fs-1 buttons">OFF</button></a>
+                                <a href="/?des=on"><button class="btn btn-outline-success fs-1 buttons">ON</button></a>
+                                <a href="/?des=off"><button class="btn btn-outline-danger fs-1 buttons">OFF</button></a>
                             </td>
                         </tr>
                         <tr>
@@ -317,14 +312,15 @@ def web_page(n):
                                 <b>! Desligar Central - SOS !</b>
                             </td>
                             <td>
-                                <a href="/?central=on"><button class="btn btn-outline-success fs-1 buttons">ON</button></a>
-                                <a href="/?central=off"><button class="btn btn-outline-danger fs-1 buttons">OFF</button></a>
+                                <a href="/?cen=on"><button class="btn btn-outline-success fs-1 buttons">ON</button></a>
+                                <a href="/?cen=off"><button class="btn btn-outline-danger fs-1 buttons">OFF</button></a>
                             </td>
                         </tr>
                     </table>
                 </body>
 
                 </html>"""
+    Cntagua()
     return html
 
 
@@ -332,69 +328,85 @@ def webserver():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', 80))
     s.listen(5)
-
+    
+    global n
+    global m
+    
     while True:
-        des_on, des_off, conn = pedido(s)
+        des_on, des_off, central_on, central_off = pedido(s, n, m)
 
-        if des_on == -1 and des_off == -1:
-            refresh(conn, -1)
-        elif des_on == 6:
-            Cnt_desHUM.value(1)
-            refresh(conn, 1)
+#         if des_on == -1 and des_off == -1:
+#             n = -1
+#             #refresh(conn, n, m)
+        if des_on == 6:
+            #Cnt_desHUM.value(1)
+            n = 1
+            #refresh(conn, n, m)
         elif des_off == 6:
-            Cnt_desHUM.value(0)
-            refresh(conn, 0)
+            #Cnt_desHUM.value(0)
+            n = 0
+            #refresh(conn, n, m)
 
-
+#         if central_on == -1 and central_off == -1:
+#             m = -1
+#             #refresh(conn, n, m)
+        if central_on == 6:
+            #Cnt_Central.value(1)
+            m = 1
+            #refresh(conn, n, m)
+        elif central_off == 6:
+            #Cnt_Central.value(0)
+            m = 0
+        #refresh(n, m)
+            
+            
 def Cntagua():
     global cnt
-    global soma
+    global soma1,soma2, soma3
     global tempoTotal1
     global tempoTotal2
-    global Estado_EletroValvula
+    global time_start
 
     time.sleep(0.01)  # delay para a leitura atuar na iteração atual.
     # Pouco caudal, bombas funcionam em alternancia
-    if Cnt_Agua_sys.read() < 1500 and Estado_EletroValvula == 1:
-        Cnt_EletroValvula.value(1)
+    if Cnt_Agua_sys.read() < 1500 and Estado_EletroValvula == 0:
+        Cnt_EletroValvula.value(Estado_EletroValvula)
         if cnt == 1:
-            time_start = rtc.datetime()
             # Motor_1 = Pin(14, Pin.OUT)  # Contacto auxiliar - Motor 1
             Motor_1.value(0)
             Motor_2.value(1)
-            time.sleep(1)
             time_actual = rtc.datetime()
-            soma = soma + abs(time_actual[6] - time_start[6])
-            tempoTotal1 = tempoTotal1 + soma
-            if soma > 5:
-                cnt = 0
-                soma = 0
-        if cnt == 0:
-            time_start = rtc.datetime()
+            soma1 = soma1 + abs(time_actual[6] - time_start[6])
+            tempoTotal1 = tempoTotal1 + soma1
+                
+        elif cnt == 0:
             Motor_1.value(1)
             Motor_2.value(0)
-            time.sleep(1)
             time_actual = rtc.datetime()
-            soma = soma + abs(time_actual[6] - time_start[6])
-            tempoTotal2 = tempoTotal2 + soma
-            if soma > 5:
-                cnt = 1
-                soma = 0
+            soma2 = soma2 + abs(time_actual[6] - time_start[6])
+            tempoTotal2 = tempoTotal2 + soma2
+        
+        if  soma1 > 5:
+            cnt = 0
+            soma1 = 0
+            
+        if  soma2 > 5:
+            cnt = 1
+            soma2 = 0
+                
     # Muito caudal, funcionam as duas bombas em conjunto
-    if Cnt_Agua_sys.read() > 1500 and Estado_EletroValvula == 1:
+    if Cnt_Agua_sys.read() > 1500 and Estado_EletroValvula == 0:
         Motor_1.value(1)
         Motor_2.value(1)
-        tempoTotal1 = tempoTotal1 + 1
-        tempoTotal2 = tempoTotal2 + 1
-        soma = 0
-        cnt = 0
-        print('STADO', Estado_EletroValvula)
-        time.sleep(1)
+        time_actual = rtc.datetime()
+        soma3 = soma3 + abs(time_actual[6] - time_start[6])
+        tempoTotal1 = tempoTotal1 + soma3
+        tempoTotal2 = tempoTotal2 + soma3
 
-    if Estado_EletroValvula == 0:  # Depósito vazio, eletroválvula accionada, bombas paradas - sistema sanitário alimentado pela rede
-        Cnt_EletroValvula.value(0)
+    if Estado_EletroValvula == 1:  # Depósito vazio, eletroválvula accionada, bombas paradas - sistema sanitário alimentado pela rede
+        Cnt_EletroValvula.value(Estado_EletroValvula)
 
-
+    
 ########################################
 # Sinalização dos contactos auxiliares #
 ########################################
@@ -403,14 +415,14 @@ def Cntagua():
 
 
 def CntAux():
-    if Cnt_Dis_C1.value() == 0:
+    if Cnt_Dis_C1.value() == 1:
         print('Possível avaria MOTOR1')
-    else:
+    elif Cnt_Dis_C1.value() == 0:
         print('Motor 1 OK')
 
-    if Cnt_Dis_C2.value() == 0:
+    if Cnt_Dis_C2.value() == 1:
         print('Possível avaria MOTOR2')
-    else:
+    elif Cnt_Dis_C2.value() == 0:
         print('Motor 2 OK')
 
 
@@ -419,7 +431,3 @@ def CntAux():
 ######################################
 while True:
     webserver()
-    # Read_BME(0)
-    # nivel()
-    # Cntagua()
-    # CntAux()
